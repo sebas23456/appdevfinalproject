ec9bc59d606dee261afcc3b73b924f77e5f8368f
Sebas23456 <smarinescuny@gmail.com>
2022-04-11 09:32:44 -0400

    I added some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -17,72 +17,48 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title:  const Text("Login Page"), centerTitle: true,
         ),
+            body:const MyHomePage(),
         ),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text("LOGO", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 14)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('assets/images/AppDevFinalLogo.jpg', height: 200, width: 400,),
+        ),
+
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text("User Name:", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 14)),
+        ),
+
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text("Password:", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 14)),
+        ),
+
+
+      ],
     );
   }
 }
